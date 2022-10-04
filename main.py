f624349ef61887737659f6a6a67f03a6657204c2
Husam El <113930359+fudgeadool@users.noreply.github.com>
2022-10-04 17:36:23 +0000

    stuff

---
+++ main.py
@@ -0,0 +1,29 @@
+# # fork the repl.it and create a new git hub repo entitled
+# #Comparison operators
+# # Logical operators
+# # Decision making
+# # loops(for loops, while loops, range, enumerator)
+# # min/max practice
+# # Random in python
+# # List comprehension
+# #importing modules
+# #modulizing your code
+# #work on creating and understanding functions
+
+# from decision_making import decisions
+# from comparison_operators import comparison
+# # from dictionaries_loops import dictionaries_loops
+from loops import loop
+
+# decisions()
+# comparison()
+loop()
+# dictionarie()
+# enumerator()
+# comprehension()
+# logical()
+# min()
+# random()
+# ranges()
+# while_loops()
+# zip()
